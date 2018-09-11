3fd77e0eab80892593021721a68a55c7439def1f
Taylork321 <taylork2142@gmail.com>
2018-09-11 14:39:36 +1200

    Initial commit

---
+++ SoftwareProcess2018SCRUM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SoftwareProcess2018SCRUM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
